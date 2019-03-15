8233da7e625c900fcdc889de59ce753d8992fe2b
YongWook-Ha <ywha12@gmail.com>
2019-03-15 17:59:18 +0900

    update: dataset

--- demo.py
+++ demo.py
@@ -1,17 +1,23 @@
-import pandas as pd
+import numpy as np
 from sklearn import linear_model
+import matplotlib
 import matplotlib.pyplot as plt
 
 #read data
-dataframe = pd.read_fwf('brain_body.txt')
-x_values = dataframe[['Brain']]
-y_values = dataframe[['Body']]
+data = np.genfromtxt("fire_and_theft.txt", names=('fire','theft'), dtype=np.float64, skip_header=1)
+x_values = [[x[0]] for x in data]
+y_values = [[x[1]] for x in data]
 
 #train model on data
 body_reg = linear_model.LinearRegression()
 body_reg.fit(x_values, y_values)
 
 #visualize results
-plt.scatter(x_values, y_values)
-plt.plot(x_values, body_reg.predict(x_values))
+
+plt.scatter(x_values, y_values,  color='black')
+plt.plot(x_values, body_reg.predict(x_values), color='blue', linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
 plt.show()
